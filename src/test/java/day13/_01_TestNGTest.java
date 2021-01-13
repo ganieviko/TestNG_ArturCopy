81e7dac8a7ac34b66f362d5d824fa7ca27ceeafd
daulet2030 <daulet2030@gmail.com>
2021-01-13 07:11:21 +0600

    day13 failing test

--- src/test/java/day13/_01_TestNGTest.java
+++ src/test/java/day13/_01_TestNGTest.java
@@ -4,7 +4,13 @@ import org.testng.annotations.Test;
 
 public class _01_TestNGTest {
     @Test
-    public void test() {
+    public void test1() {
+        // automatic exception handling
+        // any exception here will fail the test
+    }
 
+    @Test
+    public void test2() throws Exception {
+        throw new Exception();
     }
 }
