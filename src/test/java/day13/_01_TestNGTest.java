7e1aa1564f3afd0e8c90e5d61465353f1f3cf73f
daulet2030 <daulet2030@gmail.com>
2021-01-13 07:19:22 +0600

    day13 failing test

--- src/test/java/day13/_01_TestNGTest.java
+++ src/test/java/day13/_01_TestNGTest.java
@@ -1,16 +1,35 @@
 package day13;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class _01_TestNGTest {
     @Test
     public void test1() {
-        // automatic exception handling
-        // any exception here will fail the test
+        // each test case should have at least one Assertion
+        Assert.assertEquals("test", "test");
     }
 
     @Test
-    public void test2() throws Exception {
+    public void test2() {
+        Assert.assertEquals("not test", "test");
+    }
+
+    @Test
+    public void test3() throws Exception {
+        // automatic exception handling
+        // any exception here will fail the test
+
+        // something wrong in the code
         throw new Exception();
+    }
+
+    @Test
+    public void test4() throws Exception {
+        // something wrong in the business logic (software under test)
+        boolean someCondition = true;
+        if (someCondition) {
+            Assert.fail("Test failed because of some condition");
+        }
     }
 }
