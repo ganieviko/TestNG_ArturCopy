3a4ebad2e03f3d0af9f898ae5248e882fc365362
daulet2030 <daulet2030@gmail.com>
2021-01-16 06:20:49 +0600

    day 15, simple testng xml

---
+++ src/test/java/day15/_01_TestNGXML/CountryTests.java
@@ -0,0 +1,18 @@
+package day15._01_TestNGXML;
+
+import org.testng.annotations.Test;
+
+public class CountryTests {
+    @Test
+    public void testCase1(){
+        System.out.println("Country test case 1");
+    }
+    @Test
+    public void testCase2(){
+        System.out.println("Country test case 2");
+    }
+    @Test
+    public void testCase3(){
+        System.out.println("Country test case 3");
+    }
+}
