493848b0f504828066b9a1cb36125a2c8dcc6f37
daulet2030 <daulet2030@gmail.com>
2021-01-16 07:18:44 +0600

    day 15, task 1 solution

---
+++ src/test/java/day15/task1/testNgClass1.java
@@ -0,0 +1,15 @@
+package day15.task1;
+
+import org.testng.annotations.Test;
+
+public class testNgClass1 {
+    @Test
+    public void test1() {
+        System.out.println("test 1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("test 3");
+    }
+}
