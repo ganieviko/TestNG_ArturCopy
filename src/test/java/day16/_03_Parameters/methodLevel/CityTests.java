9e74750a70869a3419b26220b98a8e25ea517acc
daulet2030 <daulet2030@gmail.com>
2021-01-19 07:37:58 +0600

    day 16, parameters method level

---
+++ src/test/java/day16/_03_Parameters/methodLevel/CityTests.java
@@ -0,0 +1,21 @@
+package day16._03_Parameters.methodLevel;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class CityTests {
+    @Parameters({"name"})
+    @Test
+    public void testCase1(String name){
+        System.out.println("City test case 1 for name: " + name);
+    }
+    @Parameters({"status"})
+    @Test
+    public void testCase2(String status){
+        System.out.println("City test case 2 for status: " + status);
+    }
+    @Test
+    public void testCase3(){
+        System.out.println("City test case 3");
+    }
+}
