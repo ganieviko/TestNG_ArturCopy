50b27527b90d6a57fd52ab15a3e6bf67f10b87dd
daulet2030 <daulet2030@gmail.com>
2021-01-19 07:25:05 +0600

    day 16, include exclude test methods

---
+++ src/test/java/day16/_02_Methods/CityTests.java
@@ -0,0 +1,18 @@
+package day16._02_Methods;
+
+import org.testng.annotations.Test;
+
+public class CityTests {
+    @Test
+    public void testCase1(){
+        System.out.println("City test case 1");
+    }
+    @Test
+    public void testCase2(){
+        System.out.println("City test case 2");
+    }
+    @Test
+    public void testCase3(){
+        System.out.println("City test case 3");
+    }
+}
