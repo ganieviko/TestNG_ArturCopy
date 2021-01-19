afb9793ad1e647d967d920de029a629fd975c746
daulet2030 <daulet2030@gmail.com>
2021-01-19 06:14:34 +0600

    day 16, include

---
+++ src/test/java/day16/_01_Grouping/CityTests.java
@@ -0,0 +1,20 @@
+package day16._01_Grouping;
+
+import org.testng.annotations.Test;
+
+public class CityTests {
+    @Test(groups={"functional"})
+    public void testCase1(){
+        System.out.println("City test case functional 1");
+    }
+
+    @Test(groups={"functional", "smoke"})
+    public void testCase2(){
+        System.out.println("City test case functional and smoke 2");
+    }
+
+    @Test(groups = {"smoke"})
+    public void testCase3(){
+        System.out.println("City test case smoke 3");
+    }
+}
