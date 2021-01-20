cbb44874aeb603ac8441de2bdda98d249abc2a85
daulet2030 <daulet2030@gmail.com>
2021-01-20 08:53:26 +0600

    day 17, injection task 2 solution

---
+++ src/test/java/day17/depencyInjection/task2/CountryTests.java
@@ -0,0 +1,34 @@
+package day17.depencyInjection.task2;
+
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.xml.XmlTest;
+
+import java.lang.reflect.Method;
+
+public class CountryTests {
+    @BeforeMethod
+    public void beforeMethod(Method m) {
+        System.out.println("--------------------" + m.getName() + "--------------------");
+    }
+    @BeforeTest
+    public void beforeTest(XmlTest t) {
+        System.out.println("----------------------------------------" + t.getName() + "----------------------------------------");
+    }
+    @Test
+    public void test3() {
+        System.out.println("success");
+    }
+    @Test
+    public void test4() {
+        Assert.fail("fail");
+    }
+    @Test
+    public void test5() {
+        throw new SkipException("skipped");
+    }
+
+}
