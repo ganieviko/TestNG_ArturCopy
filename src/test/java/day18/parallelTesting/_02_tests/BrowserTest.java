c20ffd7cddcbfe005d2f743dda34fdddded1a96a
daulet2030 <daulet2030@gmail.com>
2021-01-22 08:52:16 +0600

    day 18, parallel

--- src/test/java/day18/parallelTesting/_02_tests/BrowserTest.java
+++ src/test/java/day18/parallelTesting/_02_tests/BrowserTest.java
@@ -1,17 +1,25 @@
 package day18.parallelTesting._02_tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class BrowserTest {
-    WebDriver driver;
+    private WebDriverWait wait;
+    private WebDriver driver;
+
     @Parameters("browser")
-    @Test
-    public void launchBrowser(String browserName) {
+    @BeforeClass
+    public void setUp(String browserName) {
         switch (browserName) {
             case "chrome":
                 System.setProperty("webdriver.chrome.driver", StudentConstants.CHROME_DRIVER_PATH);
@@ -27,7 +35,39 @@ public class BrowserTest {
                 break;
         }
 
-        driver.get("https://test.campus.techno.study/");
+        driver.manage().window().maximize();
+        driver.get( "https://test.campus.techno.study/" );
+        wait = new WebDriverWait(driver, 5);
     }
 
+    @AfterClass
+    public void tearDown(){
+//        driver.quit();
+    }
+
+    @Test
+    public void loginTestCase() {
+        driver.findElement(By.cssSelector("input[data-placeholder=\"Username\"]")).sendKeys("daulet2030@gmail.com");
+        driver.findElement(By.cssSelector("input[data-placeholder=\"Password\"]")).sendKeys("TechnoStudy123@");
+        driver.findElement(By.cssSelector("button[aria-label=\"LOGIN\"]")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("svg[data-icon=\"bars\"]")));
+    }
+
+    @Test(dependsOnMethods = {"loginTestCase"})
+    public void menuNavigationTest() {
+        String parentMenu = ".group-items > :nth-child(1)";
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(parentMenu)));
+        driver.findElement(By.cssSelector(parentMenu)).click();
+
+        String secondLevelMenu = " > .children > :nth-child(1)";
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(parentMenu + secondLevelMenu)));
+        driver.findElement(By.cssSelector(parentMenu + secondLevelMenu)).click();
+
+        String thirdLevelMenu = " > .children > :nth-child(1)";
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(parentMenu + secondLevelMenu + thirdLevelMenu)));
+        driver.findElement(By.xpath("//*[text()='Countries']")).click();
+
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("toolbar h3"), "Countries"));
+        Assert.assertEquals(driver.findElement(By.cssSelector("toolbar h3")).getText(), "Countries");
+    }
 }
