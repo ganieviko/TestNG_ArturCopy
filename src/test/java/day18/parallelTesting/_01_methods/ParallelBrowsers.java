e2f11f961971ce24201fdad3bfe32195f66c72c9
daulet2030 <daulet2030@gmail.com>
2021-01-22 07:30:37 +0600

    day 18, parallel methods

---
+++ src/test/java/day18/parallelTesting/_01_methods/ParallelBrowsers.java
@@ -0,0 +1,34 @@
+package day18.parallelTesting._01_methods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class ParallelBrowsers {
+    @Test
+    public void chromeBrowser() {
+        System.setProperty("webdriver.chrome.driver", StudentConstants.CHROME_DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get( "https://test.campus.techno.study/" );
+    }
+
+    @Test
+    public void firefoxBrowser() {
+        System.setProperty("webdriver.gecko.driver", StudentConstants.GECKO_DRIVER_PATH);
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://test.campus.techno.study/");
+    }
+
+    @Test
+    public void edgeBrowser() {
+        System.setProperty("webdriver.edge.driver", StudentConstants.EDGE_DRIVER_PATH);
+        WebDriver driver = new EdgeDriver();
+
+        driver.get("https://test.campus.techno.study/");
+    }
+
+}
